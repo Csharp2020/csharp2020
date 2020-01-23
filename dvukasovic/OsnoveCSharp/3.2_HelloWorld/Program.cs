e9eefb36dc07b4c75c0dd57e44eebd5de872c8d4
Dino Vukasovic <dino.vukasovic@yahoo.com>
2020-01-23 20:03:41 +0100

    Dino broj 1

---
+++ dvukasovic/OsnoveCSharp/3.2_HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _3._2_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
