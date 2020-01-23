475b21b4283bc819edfa0ee7ddb223b8d1dc5aae
Marko Curic <mcuric1710@gmail.com>
2020-01-23 21:35:28 +0100

    trtrtrt

--- mcuric/OsnoveCsharp/3.2_HelloWorld/Program.cs
+++ mcuric/OsnoveCsharp/3.2_HelloWorld/Program.cs
@@ -8,6 +8,17 @@ namespace _3._2_HelloWorld
         {
             Console.WriteLine("Hello World!");
             Console.ReadKey();
+            int a, b;
+            a = Console.Read();
+            b = Console.Read();
+            float c = Zbroj(a, b);
+            Console.WriteLine(c);
+            Console.ReadKey();
+        }
+
+        static float Zbroj(int a, int b)
+        {
+            return a + b;
         }
     }
 }
