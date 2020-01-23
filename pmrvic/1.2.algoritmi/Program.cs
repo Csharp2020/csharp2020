44cd37731a24b62bed2160a29ac770c2156c98a3
mrvic <mrvic.predrag@gmail.com>
2020-01-23 21:55:42 +0100

    zbroj a i b

--- pmrvic/1.2.algoritmi/Program.cs
+++ pmrvic/1.2.algoritmi/Program.cs
@@ -6,7 +6,26 @@ namespace _1._2.algoritmi
     {
         static void Main(string[] args)
         {
+            /*
+             * Ovo je blok komentar
+             * */
+            // Ovo je linijski komentar
             Console.WriteLine("Hello World!");
+            float a, b;
+            Console.WriteLine("Unesi prvi broj");
+            a = float.Parse(Console.ReadLine());
+            Console.WriteLine("Unesi drugi broj");
+            b = float.Parse(Console.ReadLine());
+
+            float c = Zbroj(a, b);
+            Console.WriteLine(c);
+
+        }
+
+        private static float Zbroj(float a, float b)
+        {
+            return a + b;
+
         }
     }
 }
