19158e3093fd2d7669e7ebe5481e5d5176ef58f7
Marko Curic <mcuric1710@gmail.com>
2020-01-27 19:24:09 +0100

    My first commit

---
+++ mcuric/3.2.HelloWorld/3.2.HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _3._2.HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+          
+        }
+    }
+}
