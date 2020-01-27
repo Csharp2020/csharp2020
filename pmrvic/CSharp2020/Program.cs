77892846c2e542ee8a43bc268042a0ec29243bed
mrvic <mrvic.predrag@gmail.com>
2020-01-27 20:54:25 +0100

    Dodana klasa automobil

--- pmrvic/CSharp2020/Program.cs
+++ pmrvic/CSharp2020/Program.cs
@@ -7,6 +7,11 @@ namespace CSharp2020
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Automobil A1 = new Automobil();
+            A1.MarkaAutomobila = "Pontiac";
+            A1.MarkaAutomobila2 = "Lada";
+            Console.WriteLine(A1.MarkaAutomobila2);
+            Console.WriteLine(A1.MarkaAutomobila);
         }
     }
 }
