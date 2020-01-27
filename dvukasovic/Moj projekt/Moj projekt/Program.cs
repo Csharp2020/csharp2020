be499166f2fdf0909d7bb723ea72bf76da5e5837
DinoVukasovic <dino.vukasovic@yahoo.com>
2020-01-27 19:43:00 +0100

    Ovo je moj commit

---
+++ dvukasovic/Moj projekt/Moj projekt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Moj_projekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
