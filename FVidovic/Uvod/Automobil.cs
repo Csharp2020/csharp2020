dc46f1ef0a1f2992d7a1b6e07575512bcd529483
filip-vidovic <vidovicfilip3@gmail.com>
2020-01-27 21:12:05 +0100

    Pokušaj s autima

---
+++ FVidovic/Uvod/Automobil.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uvod
+{
+    class Automobil
+    {
+        string markaAutomobila; // privatna varijabla , tuoa string , dostupna samo vlastitoj klasi
+
+        public string MarkaAutomobila {
+            get => markaAutomobila;     // expression bodied member property
+            set => markaAutomobila = value; }
+
+        public string MarkaAutomobila2
+        {
+            get
+            {
+                return markaAutomobila;   // obican nacin
+            }
+            set
+            {
+                this.markaAutomobila = value;
+            }
+        }
+
+        void Kreni() { }
+        void Kreni(int brzina) { }
+
+    }
+}
