2326fd04a386442e3da8cee5d6735153dcb12e19
Denis Požarko <denis.pozarko032@gmail.com>
2020-02-04 21:59:06 +0100

    04.02.2020 + update na zbirci

--- pozarko/5.2.3_velikiBroj/Program.cs
+++ pozarko/5.2.3_velikiBroj/Program.cs
@@ -6,6 +6,34 @@ namespace _5._2._3_velikiBroj
     {
         static void Main(string[] args)
         {
+            string broj=Console.ReadLine();
+            broj = broj.Replace(",", "");
+            if(broj.Length>=3)
+            {
+                Console.WriteLine("Broj je vilik");
+            }
+            else
+            {
+                Console.WriteLine("Broj nije vilik");
+            }
+
+            int n = 0;
+            int br = int.Parse(broj);
+
+            while (br > 0)
+            {
+                br /= 10;
+                n++;
+            }
+            if (n >= 3)
+            {
+                Console.WriteLine("Broj je vilik");
+            }
+            else
+            {
+                Console.WriteLine("Broj nije vilik");
+            }
+            /*
             int i = int.Parse(Console.ReadLine());
             if (i > 99)
             {
@@ -15,6 +43,7 @@ namespace _5._2._3_velikiBroj
             {
                 Console.WriteLine("Broj NIJE velik");
             }
+            */
         }
     }
 }
---
+++ pozarko/5.2.7_dobarBroj_2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _5._2._7_dobarBroj_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = int.Parse(Console.ReadLine());
+            if(((i%4)==0))
+        }
+    }
+}
