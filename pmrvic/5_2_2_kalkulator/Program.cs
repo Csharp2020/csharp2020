c242ebeff1221eb3aa16c13ae333f8bcd4372e5c
Predrag Mrvic <mrvic.predrag@gmail.com>
2020-02-04 21:59:40 +0100

    granjanje 5

--- pmrvic/5_2_2_kalkulator/Program.cs
+++ pmrvic/5_2_2_kalkulator/Program.cs
@@ -10,7 +10,32 @@ namespace _5_2_2_kalkulator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Unesite prvi broj!");
+            float prvi = float.Parse(Console.ReadLine());
+            Console.WriteLine("Unesite operaciju!");
+            string operacija = Console.ReadLine();
+            Console.WriteLine("Unesite drugi broj");
+            float drugi = float.Parse(Console.ReadLine());
+
+            if (operacija == "+")
+            {
+                Console.WriteLine("Operacija zbrajanja vaših unesenih brojeva daje rezultat: {0}!!", prvi + drugi);
+            }
+            else if (operacija == "-")
+            {
+                Console.WriteLine("Operacija oduzimanja vaših unesenih brojeva daje rezultat: {0}!!", prvi - drugi);
+            }
+            else if (operacija == "*")
+            {
+                Console.WriteLine("Operacija mnozenja vaših unesenih brojeva daje rezultat: {0}!!", prvi * drugi);
+            }
+            else if (operacija == "/")
+            {
+                Console.WriteLine("Operacija dijeljenja vaših unesenih brojeva daje rezultat: {0}!!", prvi / drugi);
+            }
+            else
+                Console.WriteLine("Niste unijeli ispravnu operaciju!!");
+            Console.ReadLine();
         }
     }
 }
