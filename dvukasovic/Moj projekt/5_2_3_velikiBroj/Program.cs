ac76f1cd2bdeb7580ac8b8aee8e1d068b36838fa
office10 <office10@ucione.local>
2020-02-04 21:59:27 +0100

    Utorak

---
+++ dvukasovic/Moj projekt/5_2_3_velikiBroj/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _5_2_3_velikiBroj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesibroj");
+            double a = double.Parse(Console.ReadLine);
+            if (a >= 100)
+            {
+                Console.WriteLine("Broj je veci ili jednako 100");
+            }
+            else
+            {
+                Console.WriteLine("Broj je manji od 100");
+            }
+        }
+    }
+}
