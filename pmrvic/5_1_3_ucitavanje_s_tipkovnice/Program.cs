cc147bc364ab205bfba4e8d4097728927ffbf204
pustensalanca@gmail.com <pustensalanca@gmail.com>
2020-02-04 20:11:53 +0100

    moj primjer

---
+++ pmrvic/5_1_3_ucitavanje_s_tipkovnice/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _5_1_3_ucitavanje_s_tipkovnice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesi radijus kruga");
+            int a; //deklaracija
+
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Unio si radijus"+a+" površina je:"+(a*a*3.14)+" opseg je "+(2*a*3,14));
+        }
+    }
+}
