164b9341bd0860146374d3ea94494ae123cc67b6
Denis Požarko <denis.pozarko032@gmail.com>
2020-02-04 21:22:36 +0100

    Predavanje 04.02.2020. + nekoliko zadataka iz zbirke

---
+++ pozarko/5.2.3_velikiBroj/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _5._2._3_velikiBroj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = int.Parse(Console.ReadLine());
+            if (i > 99)
+            {
+                Console.WriteLine("Broj je velik");
+            }
+            else
+            {
+                Console.WriteLine("Broj NIJE velik");
+            }
+        }
+    }
+}
---
+++ pozarko/5.2.4_xZnamenkastiBroj/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _5._2._4_xZnamenkastiBroj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = int.Parse(Console.ReadLine());
+            if (((i >= 0) && (i < 10)) || ((i <= 0) && (i > -10))){
+                Console.WriteLine("Broj je jednoznamenkast");
+            }
+            else if (((i >= 10) && (i < 100)) || ((i <= -10) && (i > -100)))
+            {
+                Console.WriteLine("Broj je dvoznamenkast");
+            }
+            else if (((i >= 100) && (i < 1000)) || ((i <= -100) && (i > -1000)))
+            {
+                Console.WriteLine("Broj je troznamenkast");
+            }
+            else
+            {
+                Console.WriteLine("Broj je viseznamenkast");
+            }
+        }
+    }
+}
