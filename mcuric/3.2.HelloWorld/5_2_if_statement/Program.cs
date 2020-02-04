dd90801194669e15f36b6d19f72bd625d0d50fd8
Marko Curic <mcuric1710@gmail.com>
2020-02-04 21:20:35 +0100

    Napravili nekoliko primjera i rijesili prvi zadatak (kalkulator)

---
+++ mcuric/3.2.HelloWorld/5_2_if_statement/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _5_2_if_statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite prirodan broj izmedju 1 i 1,000,000");
+            int broj = int.Parse(Console.ReadLine());
+            if ((broj % 5 == 0) && (broj % 8 == 0))
+            {
+                Console.WriteLine("Uneseni broj je djeljiv i sa 5 i sa 8!!");
+            }
+            else if (broj % 5 == 0)
+            {
+                Console.WriteLine("Uneseni broj je djeljiv sa 5, ali ne i 8!!");
+            }
+            else if (broj % 8 == 0)
+            {
+                Console.WriteLine("Uneseni broj je djeljiv sa 8, ali ne i 5!!");
+            }
+            else
+            {
+                Console.WriteLine("Uneseni broj nije djeljiv ni sa 5 ni sa 8!!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
