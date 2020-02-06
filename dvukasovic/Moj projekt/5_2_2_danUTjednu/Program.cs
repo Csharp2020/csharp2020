d8542f4efd15b604d88583feb03ac099cec5b34e
office10 <office10@ucione.local>
2020-02-06 21:31:55 +0100

    cetvrtak 06.02.2020

---
+++ dvukasovic/Moj projekt/5_2_2_danUTjednu/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _5_2_2_danUTjednu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesi dan u tjednu!");
+            Console.WriteLine("pon uto sri cet pet sub ned");
+
+            string dan = Console.ReadLine();
+
+            switch (dan)
+            {
+                case "pon": Console.WriteLine("Radni dan"); break;
+                case "uto": Console.WriteLine("Radni dan"); break;
+                case "sri": Console.WriteLine("Radni dan"); break;
+                case "cet": Console.WriteLine("Radni dan"); break;
+                case "pet": Console.WriteLine("Radni dan"); break;
+                case "sub": Console.WriteLine("Vikend"); break;
+                case "ned": Console.WriteLine("Vikend"); break;
+                default:
+                    Console.WriteLine("nepoznat dan");
+                    break;
+            }
+        }
+    }
+}
