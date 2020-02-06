19ef48a17bd29fc3f7ec1be584d43281108006cf
Denis Požarko <denis.pozarko032@gmail.com>
2020-02-06 20:11:30 +0100

    Pokusaj 2

---
+++ pozarko/5.3.4_faktorijel/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _5._3._4_faktorijel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
