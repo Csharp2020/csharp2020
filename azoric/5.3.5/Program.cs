44fd2eba97125d40743d765737d507b5f949cfcf
AnđelkoZorić <60152573+andelkozoric@users.noreply.github.com>
2020-02-08 16:02:57 +0100

    zadaca

---
+++ azoric/5.3.5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _5._3._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ azoric/5.3.6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _5._3._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            for (int i = 1900; i <= 2007; i++) 
+            {
+                if (i % 4 == 0 && (i % 100 != 0 || i % 400 == 0))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+
+        }
+    }
+}
