da2489d6fcd467bd83f268a022fb2cdca5a666a9
Marko Curic <mcuric1710@gmail.com>
2020-02-10 21:34:11 +0100

    The end of todays working day

---
+++ mcuric/3.2.HelloWorld/6_1_niz/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _6_1_niz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string[] nizBrojeva = new string[5] { "Jedan", "Dva", "TRI", "Cetiri", "pET" };
+        }
+    }
+}
