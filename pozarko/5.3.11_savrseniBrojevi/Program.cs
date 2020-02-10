3a368288c4a4b8f3f363573d284169271e308fcf
Denis Požarko <denis.pozarko032@gmail.com>
2020-02-10 21:33:30 +0100

    10.02.2020.

--- pozarko/5.3.11_savrseniBrojevi/Program.cs
+++ pozarko/5.3.11_savrseniBrojevi/Program.cs
@@ -7,23 +7,21 @@ namespace _5._3._11_savrseniBrojevi
         static void Main(string[] args)
         {
             int n = 0;
-            int x = 0;
-            int j = 0;
-            for(int i = 1; i < 1000; i++)
+            for(int i = 1; i <= 1000; i++)
             {
-                do
+                n = 0;
+                for(int j = 1; j < i; j++)
                 {
-                    Console.WriteLine("i: {0}, j: {1}", i, j);
                     if (i % j == 0)
                     {
                         n += j;
-                        if (n == i)
-                        {
-                            Console.WriteLine("Broj je savrseni broj i iznosi: " + n);
-                        }
+
                     }
-                    j++;
-                }while(j < i);
+                }
+                if (i == n)
+                {
+                    Console.WriteLine("Broj je savrsen i iznosi: " + n);
+                }
             }
             Console.WriteLine("n je jednak: " + n);
         }
