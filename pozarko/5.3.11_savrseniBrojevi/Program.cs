e511ce7f7419924538332c5f1cd3dfc53dce6398
denisvu99 <denis.pozarko032@gmail.com>
2020-02-10 17:31:09 +0100

    Nedovrsena zadaca

---
+++ pozarko/5.3.11_savrseniBrojevi/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _5._3._11_savrseniBrojevi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            int x = 0;
+            int j = 0;
+            for(int i = 1; i < 1000; i++)
+            {
+                do
+                {
+                    Console.WriteLine("i: {0}, j: {1}", i, j);
+                    if (i % j == 0)
+                    {
+                        n += j;
+                        if (n == i)
+                        {
+                            Console.WriteLine("Broj je savrseni broj i iznosi: " + n);
+                        }
+                    }
+                    j++;
+                }while(j < i);
+            }
+            Console.WriteLine("n je jednak: " + n);
+        }
+    }
+}
--- pozarko/5.3.9_djelitelji/Program.cs
+++ pozarko/5.3.9_djelitelji/Program.cs
@@ -1,18 +1,19 @@
 ﻿using System;
 
-namespace _5._3._4_faktorijel
+namespace _5._3._9_djelitelji
 {
     class Program
     {
         static void Main(string[] args)
         {
             int x = int.Parse(Console.ReadLine());
-            int z=1;
-            for(int i = 1; i <= x; i++)
+            for(int i=0; i <= x; i++)
             {
-                z *= i;
+                if (x % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
             }
-            Console.WriteLine(z);
         }
     }
 }
