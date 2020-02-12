c998254f0945a10191a36f80be22d942a7ee24be
Denis Požarko <denis.pozarko032@gmail.com>
2020-02-12 21:21:08 +0100

    12.02.2020. num2

---
+++ pozarko/Pr6.1.4_parNepar2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+
+namespace Pr6._1._4_parNepar2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ArrayList arr = new ArrayList();
+            for (int i = 0; i < 10; i++)
+            {
+                arr.Add(Console.ReadLine());
+            }
+
+        }
+    }
+}
