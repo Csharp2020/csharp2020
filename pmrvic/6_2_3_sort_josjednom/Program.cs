5eff9fce330a163d47e5c348aeb324e26cb7f7d2
office10 <office10@ucione.local>
2020-02-12 21:51:59 +0100

    dodan projekt list

---
+++ pmrvic/6_2_3_sort_josjednom/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _6_2_3_sort_josjednom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> prirodni = new List<int>();
+            int broj;
+            do
+            {
+              broj = int.Parse(Console.ReadLine());
+          prirodni.Add(broj);
+
+            } while (broj != 0);
+
+            prirodni.Sort();
+
+            foreach (var item in prirodni)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
