bb490a92446ecb591a0d26e7b0b2b2b87a0d11e8
Marko Curic <mcuric1710@gmail.com>
2020-02-12 20:03:29 +0100

    _6_1_3

---
+++ mcuric/3.2.HelloWorld/6_1_2_max/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace _6_1_2_max
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite 5 brojeva!");
+            int[] brojevi = new int[5];
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Unesite {0}. broj: ", i+1);
+                brojevi[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Najveci broj medju navedenima je {0} i nalazi se na {1}. poziciji", brojevi.Max(), Array.IndexOf(brojevi, brojevi.Max()) );
+            for(int i = 0; i < brojevi.Length; i++)
+            {
+                if (i == Array.IndexOf(brojevi, brojevi.Max()))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write(brojevi[i] + " ");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+                else Console.Write(brojevi[i] + " ");
+            }
+            int pomoc;
+            pomoc = brojevi.Max();
+            brojevi[Array.IndexOf(brojevi, brojevi.Max())] = brojevi.Min();
+            brojevi[Array.IndexOf(brojevi, brojevi.Min())] = pomoc;
+
+            Console.WriteLine("Isti Array sa zamjenjenim mjestima najveceg i najmanjeg broja: ");
+            foreach (var item in brojevi)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
