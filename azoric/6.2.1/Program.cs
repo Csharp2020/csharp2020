f5d58ffa8679f0b6c47c273a56d74abeb231a568
office10 <office10@ucione.local>
2020-02-12 21:22:45 +0100

    nizovi arrays

---
+++ azoric/6.2.1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _6._2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ azoric/6.2.2.nepar2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _6._2._2.nepar2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
