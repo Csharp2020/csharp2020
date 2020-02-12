d7c03e3c18f5be680f5ff9c15e231f092b2d41e5
Denis Požarko <denis.pozarko032@gmail.com>
2020-02-12 21:50:59 +0100

    12.02.2020. pt.3

--- pozarko/Pr6.1.4_parNepar2/Program.cs
+++ pozarko/Pr6.1.4_parNepar2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace Pr6._1._4_parNepar2
 {
@@ -7,13 +8,40 @@ namespace Pr6._1._4_parNepar2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            ArrayList arr = new ArrayList();
+            Console.WriteLine("Unesite 10 elemenata!");
+            List<int> arr = new List<int>();
             for (int i = 0; i < 10; i++)
             {
-                arr.Add(Console.ReadLine());
+                arr.Add(int.Parse(Console.ReadLine()));
             }
 
+            List<int> parni = new List<int>();
+            List<int> neparni = new List<int>();
+
+            foreach (var item in arr)
+            {
+                if (item % 2 == 0)
+                {
+                    parni.Add(item);
+                }
+                else
+                {
+                    neparni.Add(item);
+                }
+            }
+
+            //ispis parnih
+            Console.WriteLine("Parni:");
+            foreach (var item in parni)
+            {
+                Console.Write("{0} ", item);
+            }
+
+            Console.WriteLine("Neparni:");
+            foreach (var item in neparni)
+            {
+                Console.Write("{0} ", item);
+            }
         }
     }
 }
