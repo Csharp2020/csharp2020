730b872e2c0e678c5b7ba20b5ed34032416a2107
AnđelkoZorić <60152573+andelkozoric@users.noreply.github.com>
2020-02-14 16:55:37 +0100

    zadaca 6.2.4. i 6.2.5.

---
+++ azoric/6.2.5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+namespace _6._2._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite prirodan broj");
+            int prirodanbroj = int.Parse(Console.ReadLine());
+            ArrayList arr = new ArrayList();
+            while (prirodanbroj > 0)
+            {
+                arr.Add(prirodanbroj % 10);
+                prirodanbroj /= 10;
+            }
+            Console.WriteLine("napoako");
+            for (int i = 0; i < arr.Count; i++)
+            {
+                Console.Write(arr[i]);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ azoric/6.25/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+namespace _6._25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("unesite prirodan broj");
+            int prirodanbroj = int.Parse(Console.ReadLine());
+            int faktor = 2;
+            Console.WriteLine("{0} = ", prirodanbroj);
+
+            ArrayList arr = new ArrayList();
+            while (faktor <= prirodanbroj)
+            {
+                if (prirodanbroj % faktor == 0)
+                {
+                    arr.Add(faktor);
+                    prirodanbroj /= faktor;
+                }
+                else
+                {
+                    faktor++;
+                }
+
+            }
+            for (int i = 0; i < arr.Count; i++)
+            {
+                Console.WriteLine(arr[i]);
+                if(i < arr.Count - 1)
+                {
+                    Console.WriteLine(" * ");
+                }
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
