90adba327ef1414c473cc26e8bb0ce93914636df
denisvu99 <denis.pozarko032@gmail.com>
2020-02-14 21:21:54 +0100

    12.02.2020

--- pozarko/7.2.1_krug/Program.cs
+++ pozarko/7.2.1_krug/Program.cs
@@ -6,7 +6,18 @@ namespace _7._2._1_krug
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            float r = float.Parse(Console.ReadLine());
+            Console.WriteLine("Opseg: {0}, Povrsina: {1}",Opseg(r),Povrsina(r));
+        }
+
+        private static double Povrsina(float r)
+        {
+            return r * r * Math.PI;
+        }
+
+        private static double Opseg(float r)
+        {
+            return 2 * r * Math.PI;
         }
     }
 }
