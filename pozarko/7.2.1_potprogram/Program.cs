0605b6f14eea0980f0e7242be27aa6e598c97744
denisvu99 <denis.pozarko032@gmail.com>
2020-02-14 21:05:02 +0100

    12.02.2020

---
+++ pozarko/7.2.1_potprogram/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _7._2._1_potprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                HelloWorld();
+            }
+        }
+
+        static void HelloWorld()
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
