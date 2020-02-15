17a667d5677cb07a50cf659bd5d9f9d23c550eb2
denisvu99 <denis.pozarko032@gmail.com>
2020-02-15 05:43:36 +0100

    Izkomentirani kod sa predavanja 14.02.2020. radi pomoci izostalom kolegi

--- pozarko/7.2.1_krug/Program.cs
+++ pozarko/7.2.1_krug/Program.cs
@@ -6,16 +6,17 @@ namespace _7._2._1_krug
     {
         static void Main(string[] args)
         {
-            float r = float.Parse(Console.ReadLine());
+            int r = int.Parse(Console.ReadLine());
             Console.WriteLine("Opseg: {0}, Povrsina: {1}",Opseg(r),Povrsina(r));
         }
 
-        private static double Povrsina(float r)
+        //Prima int, vraca double(jer je PI tipa double), da nije mozemo joj staviti tip float jer ne ocekujemo cijelobrojni broj.
+        private static double Povrsina(int r)
         {
             return r * r * Math.PI;
         }
 
-        private static double Opseg(float r)
+        private static double Opseg(int r)
         {
             return 2 * r * Math.PI;
         }
--- pozarko/7.2.1_potprogram/Program.cs
+++ pozarko/7.2.1_potprogram/Program.cs
@@ -4,14 +4,37 @@ namespace _7._2._1_potprogram
 {
     class Program
     {
+        /*Radi lakseg citanja programa i reuse-anja odredenog dijela koda koristimo metode.
+        Metode ti mogu objasniti na kreativni nacin na primjeru tvornice auta.
+        Ubiti metodu Main mozes zamisliti kao dio tvornice di se sastavlja cijelii dovrsava cijeli auto.
+        Dok ostali dijelovi tvornice koriste se za proizvodnju motora, vrata, sistema za kocenje itd.*/
+
+        //U ovome primjeru u metodi Main pozivamo metodu HelloWorld
         static void Main(string[] args)
         {
             for (int i = 0; i < 10; i++)
             {
                 HelloWorld();
+
+
+
+                //OVAJ KOMENTAR PROCITAJ NAKON STO SI PROCITAO KOMENTARE IZNAD DEFINICIJE METODE HelloWorld();
+                /*Kada bi obrisao static ovako bi pozivao metodu HelloWorld (kod dolje), tako da isprobaj obrisati static
+                 kod definicije HelloWorld metode i vidjeti ces da ce ti kod poziva metode iznad ovog komentara izbacivati
+                 error.*/
+
+                /*
+                Program p = new Program(); 
+                p.HelloWorld();
+                */
             }
         }
 
+
+        /*Ovo je definicija metode HelloWorld(), ona je tipa void jer ne vraca nikakve parametre koje mozemo storeati
+         u neku varijablu. Svaka metoda koja netreba vratiti parametar definira se sa main.*/
+
+        //static ubiti tu stoji kako bi metoda bila vidljiva svim ostalim metodama u ovom namespaceu (koliko sam ja skonto)
         static void HelloWorld()
         {
             Console.WriteLine("Hello world");
