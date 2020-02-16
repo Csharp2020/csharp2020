172b103b2be9107037e11f217c1a83bf5a0120c0
danijelakules <43299323+danijelakules@users.noreply.github.com>
2020-02-16 12:12:05 +0100

    Krug-zadatak

---
+++ DanijelaK/7_2_2_jednakokracan_trokut/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _7_2_2_jednakokracan_trokut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
