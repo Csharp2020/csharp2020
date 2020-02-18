30f399e3d185b8a7efa8941bc915a1c432e639d5
Marko Curic <mcuric1710@gmail.com>
2020-02-18 21:48:45 +0100

    Zapoceli rad s klasama

---
+++ mcuric/3.2.HelloWorld/8_1_2_ucenik/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _8_1_2_ucenik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! \n");
+
+            Ucenik marko = new Ucenik("Marko", "Curic");
+            marko.mathGrade = 5;
+            marko.englishGrade = 5;
+            marko.biologyGrade = 3;
+
+            Profesor mProfesor = new Profesor("Branka","Kolar");
+            mProfesor.profession = "math";
+
+            Profesor eProfesor = new Profesor("Stefania", "Bogicevic");
+            eProfesor.profession = "english";
+
+            Profesor bProfesor = new Profesor("Doriann","Gray");
+            bProfesor.profession = "biology";
+
+            bProfesor.Odgovor(marko.BiologyGrade());
+            Console.WriteLine();
+            mProfesor.Odgovor(marko.MathGrade());
+            Console.WriteLine();
+            eProfesor.Odgovor(marko.EnglishGrade());
+
+            Console.ReadLine();
+        }
+    }
+}
