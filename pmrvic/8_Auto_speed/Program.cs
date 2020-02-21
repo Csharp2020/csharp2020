18777244fd36111edfb1b2e8eba7a66d34efd05e
mrvic <mrvic.predrag@gmail.com>
2020-02-21 14:16:58 +0100

    Dodan graf ubrzanja automobila

---
+++ pmrvic/8_Auto_speed/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _8_Auto_speed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Auto a1 = new Auto("Nissan", "Qashqai", 130);
+
+            a1.Ubrzaj(acc_level: 10, acc_time: 10);  // index ubrzanja [1-10], trajanje ubrzanja [s]
+            a1.Ubrzaj(acc_level: 1, acc_time: 10);
+            a1.Ubrzaj(acc_level: 10, acc_time: 10);
+
+            Chart.DrawChart(a1.dijagramBrzine);
+            
+            Console.ReadKey();
+
+        }
+        
+    }
+}
