774d55d68623cd8f4ee8f4d122cdd7f0b3cd3756
unknown <dino.vukasovic@yahoo.com>
2020-02-24 16:33:13 +0100

    Zadaci

---
+++ dvukasovic/Moj projekt/8_1_1/Proizvod.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _8_1_1
+{
+    class Proizvod
+    {
+        public string Naziv = "";
+        public double Cijena =0;
+        public double Marza= 0;
+        public double Porez =0;
+
+        public Proizvod()
+        {
+        }
+
+        public double MPC ()
+        {
+            return Cijena + Marza + Porez;
+        }
+    }
+}
