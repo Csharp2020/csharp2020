86ebd34475b5919a05a19119caf50eb2b5e06f17
Marko Curic <mcuric1710@gmail.com>
2020-02-24 20:10:06 +0100

    Zadaca za 24.02. obbjekti/klase/zadaci 8_1_2 i 8_1_3

---
+++ mcuric/3.2.HelloWorld/8_1_2_Automobil/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Timers;
+
+namespace _8_1_2_Automobil
+{
+    class Program
+    {
+        private static Timer aTimer;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Opisite automibil za koji zelite izracunati cijenu!");
+            Automobil a1 = new Automobil();
+            Console.WriteLine("Marka automobila: ");
+            a1.MarkaAutomobila = Console.ReadLine();
+            Console.WriteLine("KS: ");
+            a1.KS = double.Parse(Console.ReadLine());
+            Console.WriteLine("Osnovna cijena: ");
+            a1.OsnovnaCijena = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Ukupna cijena danog automobila iznosi: {a1.OsnovnaCijena + a1.OsnovnaCijena * a1.IznosPoreza()}");
+
+            // Create a timer and set a two second interval.
+            aTimer = new System.Timers.Timer();
+            aTimer.Interval = 5000;
+
+            // Hook up the Elapsed event for the timer. 
+            aTimer.Elapsed += OnTimedEvent;
+
+            // Have the timer fire repeated events (true is the default)
+            aTimer.AutoReset = true;
+
+            // Start the timer
+            aTimer.Enabled = true;
+
+            Console.WriteLine("Press the Enter key to exit the program at any time... ");
+            Console.ReadLine();
+        }
+
+        private static void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            Console.WriteLine("The Elapsed event was raised at {0}", e.SignalTime);
+        }
+    }
+}
---
+++ mcuric/3.2.HelloWorld/8_1_3_Zaposlenik/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _8_1_3_Zaposlenik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Zaposlenik dominik = new Zaposlenik("Dominik","Dodic");
+            dominik.brojBodova = 500;
+            dominik.vrijednostBoda = 80;
+            Console.WriteLine($"Bruto izracun place za osobu {dominik.ime} {dominik.prezime} : {dominik.BrutoIzracunPlace()}," +
+                $"Neto izracun place za osobu {dominik.ime} {dominik.prezime} : {dominik.NetoIzracunPlace()} ");
+            Console.ReadLine();
+        }
+    }
+}
