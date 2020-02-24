05766db15540bea9b364660e1b08ef06cca253c0
danijelakules <43299323+danijelakules@users.noreply.github.com>
2020-02-24 12:18:47 +0100

    Nova klasa proizvod - error

---
+++ DanijelaK/8_1_1_proizvod/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _8_1_1_proizvod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite naziv proizvoda:");
+            Proizvod p = new Proizvod(Console.ReadLine());
+
+            Console.WriteLine("Unesite cijenu proizvoda:");
+            p.Cijena = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Unesite maržu proizvoda:");
+            p.Marza = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Unesite Porez proizvoda:");
+            p.Porez = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Maloprodajna cijena {0} je {1}.", p.Naziv, p.MPC()); 
+        }
+
+    }
+}
---
+++ DanijelaK/8_1_1_proizvod/Proizvod.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _8_1_1_proizvod
+{
+    internal class Proizvod
+    {
+        private double cijena =0;
+        private double marza= 0;
+        private double porez= 0;
+        private string naziv= "";
+
+        public Proizvod(string n)
+        {
+            naziv = n; 
+        }
+
+        public double Cijena
+        {
+            get => cijena;
+            set => cijena;
+        }
+        public double Marza
+        {
+            get => marza;
+            set => porez;
+        }
+        public double Porez
+        {
+            get => porez;
+            set => porez;
+        }
+
+        public int Naziv { get; internal set; }
+
+        public double MPC()
+        {
+            return cijena + marza + porez;
+        }
+    }
+}
