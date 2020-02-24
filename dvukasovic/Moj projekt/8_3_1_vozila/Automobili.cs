cf69e1f9d74adcb90d2f2fa6e25c6d94182f66f9
office10 <office10@ucione.local>
2020-02-24 22:04:02 +0100

    24.2

---
+++ dvukasovic/Moj projekt/8_3_1_vozila/Automobili.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _8_3_1_vozila
+{
+    class Automobili : CollectionBase
+    {
+        internal int Add(Automobil a1)
+        {
+            return InnerList.Add(a1);
+        }
+
+        internal void Remove(Automobil a1)
+        {
+            InnerList.Remove(a1);
+        }
+
+        //ovime dobivamo mogucnost ispisa foreach
+        public Automobil this[int index]
+        {
+            get => (Automobil)InnerList[index];
+        }
+    }
+}
---
+++ dvukasovic/Moj projekt/8_3_1_vozila/Vozilo.cs
@@ -0,0 +1,9 @@
+﻿namespace _8_3_1_vozila
+{
+    internal class Vozilo
+    {
+        public string Naziv { get; internal set; }
+        public string Boja { get; internal set; }
+        public int KS { get; internal set; }
+    }
+}
