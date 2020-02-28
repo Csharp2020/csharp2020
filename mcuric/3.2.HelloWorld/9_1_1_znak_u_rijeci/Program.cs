8e9751286e9608ac31a0dc4bf405547c2e80586d
Marko Curic <mcuric1710@gmail.com>
2020-02-28 21:55:47 +0100

    radili s unosom teksta, palindrome, trazenje rijeci

---
+++ mcuric/3.2.HelloWorld/9_1_1_znak_u_rijeci/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _9_1_1_znak_u_rijeci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite rijec!");
+            string rijec = Console.ReadLine();
+            Console.WriteLine("Unesite znak koji zelite naci: ");
+            string znak = Console.ReadLine();
+            int kolikoputa = 0;
+            while(rijec.IndexOf(znak) != -1)
+            {
+                
+                rijec = rijec.Substring(rijec.IndexOf(znak) + 1);
+                kolikoputa++;
+            }
+            Console.WriteLine($"Znak {znak} se pojavljuje {kolikoputa} u rijeci {rijec} ");
+            Console.ReadLine();
+        }
+    }
+}
