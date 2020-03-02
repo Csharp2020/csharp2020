4d050eab156667b9f6a248702b63b2b4a6b84650
Marko Curic <mcuric1710@gmail.com>
2020-03-02 20:57:07 +0100

    Rijesio zadatke iz LINQ poglavlja

---
+++ mcuric/3.2.HelloWorld/11_1_linq/Automobil.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _11_1_linq
+{
+    public class Automobil
+    {
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public int Zapremina { get; set; }
+    }
+}
