1e6cd7903f18638c93a8c63b46430ba65637ca0a
unknown <dino.vukasovic@yahoo.com>
2020-03-03 17:26:22 +0100

    datoteka

---
+++ dvukasovic/Moj projekt/10_1_1_Parni/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace _10_1_1_Parni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Parni");
+
+            FileStream fs = new FileStream("Parni.txt", FileMode.Create);
+            StreamWriter sw = new StreamWriter(fs);
+
+            for (int i = 2; i <= 100; i += 2)
+            {
+                sw.WriteLine(i);
+            }
+            sw.Flush();
+            sw.Close();
+            fs.Close();
+
+            fs = new FileStream("Parni.txt", FileMode.Open);
+            StreamReader sr = new StreamReader(fs);
+            string procitaj = sr.ReadToEnd();
+            fs.Close();
+            Console.WriteLine(procitaj);
+            
+
+
+
+
+        }
+    }
+}
---
+++ dvukasovic/Moj projekt/Primjer_10_1_1_Datoteke/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Primjer_10_1_1_Datoteke
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Upišite tekst: ");
+            string upisano = Console.ReadLine();
+
+            FileStream fs1 = new FileStream("Proba.txt", FileMode.Create);
+
+            StreamWriter sw = new StreamWriter(fs1);
+
+            sw.WriteLine(upisano);
+            sw.Flush();
+            sw.Close();
+            fs1.Close();
+
+            FileStream fs2 = new FileStream("Proba.txt", FileMode.Open);
+            StreamReader sr = new StreamReader(fs2);
+
+            string procitano = sr.ReadToEnd();
+            sr.Close();
+            fs2.Close();
+
+            Console.WriteLine(procitano);
+
+        }
+    }
+}
