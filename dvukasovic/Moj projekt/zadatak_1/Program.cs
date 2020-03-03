710ae7d108be3445a7f6fb9889d3c85b34755952
Dino Vukasovic <dino.vukasovic@yahoo.com>
2020-03-03 22:02:26 +0100

    poruka

---
+++ dvukasovic/Moj projekt/zadatak_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace zadatak_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadatak 1");
+           // Ispisati svako drugo slovu stringu
+            string recenica = "Bio sam u zološkom vrtu";
+            
+            Console.WriteLine(recenica.Replace(" ", ""));
+            for (int i = 0; i < recenica.Length; i+=1)
+            {
+
+                Console.WriteLine(recenica[i].ToString());
+            }
+            
+
+           
+
+        }
+    }
+}
---
+++ dvukasovic/Moj projekt/zadatak_2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+
+namespace zadatak_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Unesi tekst "Ivica ide u skolu Antuna Antunovica"
+              3 liste -1. <list> pocinju sa slovom "a"
+              2. <list> pocinje sa slovom "i"
+              3.<list> ostale rijeci koje ne pocinju sa a i i
+              ipisi koliko koja lista ima rijci
+
+            */
+            string recenica = "Ivica ide u skolu Antuna Antunovica";
+            ArrayList a = new ArrayList();
+            ArrayList b = new ArrayList();
+            ArrayList c = new ArrayList();
+            string[] rijec = recenica.Split();
+
+            foreach (string s in rijec)
+            {
+                Console.Write(s[0] + " ");
+                if (s == "A" || s == "a")
+                {
+                    a.Add(s);
+                    Console.WriteLine(a.Count);
+                }
+                else if ( s == "I" || s == "i")
+                {
+                    b.Add(rijec);
+                }
+                
+            }
+            Console.WriteLine(a.Count);
+
+
+
+
+
+        }
+    }
+}
