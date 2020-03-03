b12eb9072fe24458ce8d5248d638e6c7c7119858
Predrag Mrvic <mrvic.predrag@gmail.com>
2020-03-03 21:42:11 +0100

    enum

---
+++ pmrvic/13_1_enum/Pas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _13_1_enum
+{
+    class Pas
+    {
+        public void vau()
+        {
+         Console.WriteLine($"Moj pas je {Dani.Srijeda} {Program.Spol.Zenski}");
+        }
+        
+    }
+}
