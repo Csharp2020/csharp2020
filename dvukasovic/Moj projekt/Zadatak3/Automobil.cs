997b6f9f7e7b54c3a2d45a0b527934dc229e976e
office10 <office10@ucione.local>
2020-03-05 21:57:36 +0100

    zadaci

---
+++ dvukasovic/Moj projekt/Zadatak3/Automobil.cs
@@ -0,0 +1,39 @@
+﻿namespace Zadatak3
+{
+    internal class Automobil
+    {
+        internal string Naziv;
+        internal int GodinaProizvodnje;
+        internal int OsnovnaCijena;
+        public double Starost()
+        {
+            return 2020 - GodinaProizvodnje;
+        }
+
+        public override string ToString()
+        {
+            return $"Automobil naziv je {Naziv} godina proizvodnje je {GodinaProizvodnje} a osnovna cijena je {OsnovnaCijena} ";
+        }
+
+        public double UkupnaCijena()
+        {
+            double postotak = 0;
+            if (OsnovnaCijena <= 70000 )
+            {
+                postotak = 1.3;
+            }
+            else if (OsnovnaCijena <= 100000)
+            {
+                postotak = 1.4;
+            }
+            else
+            {
+                postotak = 1.5;
+            }
+            return OsnovnaCijena * postotak;
+        }
+    }
+
+    
+    
+}
