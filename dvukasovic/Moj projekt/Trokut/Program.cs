c420a9421a3ce0ea0db229ef480d5fecbbf95725
unknown <dino.vukasovic@yahoo.com>
2020-03-05 16:46:51 +0100

    jos vjezbe

---
+++ dvukasovic/Moj projekt/Trokut/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Trokut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite duzinu katete a");
+            double a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Unesite duzinu katete b");
+            double b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Povrsina trokuta je {0}", Povrsina(a, b));
+        }
+
+        private static double Povrsina(double a, double b)
+        {
+            return (a * b) / 2;
+        }
+    }
+}
---
+++ dvukasovic/Moj projekt/rijecURecenici/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace rijecURecenici
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite recenicu");
+            string recenica = Console.ReadLine();
+            string[] rijec = recenica.Split();
+            Console.WriteLine("Unesite rijec koju trazimo");
+            string t = Console.ReadLine();
+            int counter = 0;
+            for (int i = 0; i < rijec.Length; i++)
+            {
+                if (rijec[i] == t)
+                {
+                    counter++;
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Broj rijeci {0} u recenici {1} je {2}", t, recenica, counter);
+        }
+    }
+}
