7f9cfb204d6333c01e0a01ef56a8304a7704f22f
danijelakules <43299323+danijelakules@users.noreply.github.com>
2020-03-31 21:43:22 +0200

    Kontroleri

---
+++ DanijelaK/MVC/Mvc_Razvoj/3_kontrolori/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _3_kontrolori.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
