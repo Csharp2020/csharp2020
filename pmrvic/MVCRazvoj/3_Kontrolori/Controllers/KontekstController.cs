aa9b96affe0fb0891c10d0df6b583954d29e9caa
mrvic <mrvic.predrag@gmail.com>
2020-03-31 21:40:36 +0200

    Podaci iz querystringa CORE

---
+++ pmrvic/MVCRazvoj/3_Kontrolori/Controllers/KontekstController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _3_Kontrolori.Controllers
+{
+    public class KontekstController : Controller
+    {
+        public IActionResult Index()
+        {
+            DateTime datum = DateTime.Now;
+
+            return View((object)datum);
+        }
+        
+        /**
+         * Nova sintaksa .NET CORE
+         * */
+        public IActionResult QueryPodaci(
+            [FromQuery(Name = "ime")] string ime
+            , [FromQuery(Name = "prezime")] string prezime)
+        {
+            // citaj ime i prezime iz querystringa
+            // stara sintaksa is ASP.NET
+            //Microsoft.AspNetCore.Http.QueryString qs =Request.QueryString;
+            //= Request.QueryString["ime"];
+            
+
+            List<string> osobnoimeiprezime = new List<string>();
+            osobnoimeiprezime.Add(ime);
+            osobnoimeiprezime.Add(prezime);
+            return View((object)osobnoimeiprezime);
+        }
+
+    }
+}
