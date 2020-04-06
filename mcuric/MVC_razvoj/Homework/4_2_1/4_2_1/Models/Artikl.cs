0e517a69458ee3a16a2fc8b68b4b44b5d7606865
Marko Curic <mcuric1710@gmail.com>
2020-04-06 20:53:53 +0200

    Primjeri s validacijom koje smo komentirali

---
+++ mcuric/MVC_razvoj/Homework/4_2_1/4_2_1/Models/Artikl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _4_2_1.Models
+{
+    public class Artikl
+    {
+        public string Kategorija { get; set; }
+        public string Naziv { get; set; }
+        public decimal Cijena { get; set; }
+        public int Kolicina { get; set; }
+    }
+}
