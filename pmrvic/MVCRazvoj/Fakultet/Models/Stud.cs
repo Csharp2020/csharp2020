6264523e707f8004d5f066b2364e7964accc4f03
mrvic <mrvic.predrag@gmail.com>
2020-04-14 20:37:24 +0200

    Scaffold-DbContext 'Server=POWERRYZEN\SQLEXPRESS;Initial Catalog=Fakultet;Integrated Security=True;' Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models

---
+++ pmrvic/MVCRazvoj/Fakultet/Models/Stud.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fakultet.Models
+{
+    public partial class Stud
+    {
+        public int MbrStud { get; set; }
+        public string ImeStud { get; set; }
+        public string PrezStud { get; set; }
+        public int? PbrRod { get; set; }
+        public int PbrStan { get; set; }
+        public DateTime? DatRodStud { get; set; }
+        public string JmbgStud { get; set; }
+
+        public virtual Mjesto PbrRodNavigation { get; set; }
+        public virtual Mjesto PbrStanNavigation { get; set; }
+    }
+}
