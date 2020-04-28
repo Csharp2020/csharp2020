4413a52af17e44f408853dba66dc16591c400f5c
Danijela Kuleš <danijela.kules@gmail.com>
2020-04-28 19:45:50 +0200

    Query PRED-NASTAVNIK

--- DanijelaK/MVC/Fakultet/Fakultet/RANDOM.sql
+++ DanijelaK/MVC/Fakultet/Fakultet/RANDOM.sql
@@ -2,12 +2,7 @@ USE fakultet
 GO
 
 INSERT INTO fakultet.dbo.pred_nastavnik(SifNastavnik,SifPred) 
-VALUES (150, (SELECT TOP (1) pred.sifPred FROM [fakultet].[dbo].[pred] as pred ORDER BY NEWID()),
-(SELECT TOP (1) nast.sifNastavnik FROM [fakultet].[dbo].[nastavnik] as nast ORDER BY NEWID()));
-
-
-
-INSERT INTO [fakultet].[dbo].[pred_nastavnik](SifNastavnik,SifPred)
-VALUES (150, (SELECT TOP (1) pred.sifPred, (SELECT TOP (1) nastavnik.sifNastavnik FROM [fakultet].[dbo].[nastavnik] as nastavnik ORDER BY NEWID())
-FROM [fakultet].[dbo].[pred] as pred ORDER BY NEWID()));
+VALUES ((SELECT TOP (1) [sifNastavnik] FROM [fakultet].[dbo].nastavnik ORDER BY NEWID()),
+(SELECT TOP (1) [sifPred] FROM [fakultet].[dbo].[pred] ORDER BY NEWID()))
+GO
 
