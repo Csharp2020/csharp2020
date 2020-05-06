c1d8377f5e435db4ec12c37b4f4116fa78d23c3a
Danijela Kuleš <danijela.kules@gmail.com>
2020-05-06 19:29:51 +0200

    Login

---
+++ DanijelaK/MVC/Fakultet/testAuth/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace testAuth.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
