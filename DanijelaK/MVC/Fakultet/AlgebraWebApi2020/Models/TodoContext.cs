4af181256146d81a5c30978e7ed93319135973b7
Danijela Kuleš <danijela.kules@gmail.com>
2020-05-06 21:17:07 +0200

    CROD

---
+++ DanijelaK/MVC/Fakultet/AlgebraWebApi2020/Models/TodoContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlgebraWebApi2020.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
+
--- DanijelaK/MVC/Fakultet/AlgebraWebApi2020/Startup.cs
+++ DanijelaK/MVC/Fakultet/AlgebraWebApi2020/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+
 
 namespace AlgebraWebApi2020
 {
@@ -25,7 +27,10 @@ namespace AlgebraWebApi2020
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<Models.TodoContext>(opt =>
+               opt.UseInMemoryDatabase("TodoList"));
             services.AddControllers();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
