492cd76a5fc1505266975d1167a004d3d2ffbf78
mrvic <mrvic.predrag@gmail.com>
2020-05-06 20:10:07 +0200

    kreiran kontroler i model todoitem

--- pmrvic/MVCRazvoj/AlgebraWebAPI2020/Startup.cs
+++ pmrvic/MVCRazvoj/AlgebraWebAPI2020/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +26,9 @@ namespace AlgebraWebAPI2020
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<Models.TodoContext>(opt =>
+              opt.UseInMemoryDatabase("TodoList"));
+            services.AddControllers();
             services.AddControllers();
         }
 
