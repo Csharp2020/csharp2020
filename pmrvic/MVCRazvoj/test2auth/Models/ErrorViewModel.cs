0a9b188b404b4f8f23eb63ffd86babe5735d02bd
mrvic <mrvic.predrag@gmail.com>
2020-05-06 19:28:59 +0200

    dodane stranice za manage usera

---
+++ pmrvic/MVCRazvoj/test2auth/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace test2auth.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
